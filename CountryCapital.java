2daf923886f15285f29658074aba22ff61864e11
NITHIN5634 <businityanandareddy@gmail.com>
2025-06-05 12:11:53 +0530

    Add files via upload

---
+++ CountryCapital.java
@@ -0,0 +1,8 @@
+package javaprograms;
+import java.uti.Map;
+import java.util.TreeMap;
+Public class CountryCapital{
+	public static void main(String[] args){
+		TreeMap<String,String>countryCapital=new TreeMap<String,String>();
+		
+		countryCapital
